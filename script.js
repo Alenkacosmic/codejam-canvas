48342dd9a9115240f6e90b4d2671beaf1d813a8c
Elena Capienko <elena.capienko@gmail.com>
2019-10-27 13:33:43 -0400

    feat: add last image; add links

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-const canvas = document.getElementById('switcher'); // see more information here https://www.youtube.com/watch?v=1NQE6vcVK6M 
-    canvas.width = 512;                                                             // https://www.youtube.com/watch?v=dYjSV5UUxJg 
-    canvas.height = 512;                                                            // https://www.youtube.com/watch?v=0ZJ1Ev7qgmM
-let context = canvas.getContext('2d');
+const canvas = document.getElementById('switcher');
+    canvas.width = 512;
+    canvas.height = 512;
+const context = canvas.getContext('2d');
 
-let small_img = [
-    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-];
-for (let i=0; i<small_img.length; i++) {
-    for (let j=0; j<small_img[i].length; j++) {
-        context.fillStyle = "#" + small_img[i][j];
-        context.fillRect(i*128, j*128, 128, 128);
-    }     
-}
+document.querySelector('#canv_small').addEventListener('click', function() { 
+    fetch("https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json")
+        .then(response => response.json())
+        .then(json => {
+            small = json;
+            for (let i = 0; i < small.length; i++) {
+                for (let j = 0; j < small.length; j++) {
+                    context.fillStyle = '#' + small[i][j];
+                    context.fillRect(i*128, j*128, 128, 128);
+                }
+            };
+        });
+});
+
+document.querySelector('#canv_medium').addEventListener('click', function() { 
+    fetch("https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json")
+        .then(response => response.json())
+        .then(json => {
+            medium = json;
+            for (let i = 0; i < medium.length; i++) {
+                for (let j = 0; j < medium.length; j++) {
+                    medium[i][j][3] = medium[i][j][3] * 100 / 255;
+                    context.fillStyle = 'rgba' + '(' + medium[i][j].join() + ')';
+                    context.fillRect(i*16, j*16, 16, 16);
+                }
+            }
+        });
+});
+
+document.querySelector('#canv_icon').addEventListener('click', function() {
+    let icon = new Image();
+    icon.src = './assets/images/rss_icon.png';
+    icon.onload = function() {
+        context.drawImage(icon, 0, 0, 512, 512);
+    };
+});
