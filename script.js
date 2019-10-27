6ca48ee2e61333f1bf98ca29f37ab3944bf6910f
Elena Capienko <elena.capienko@gmail.com>
2019-10-27 09:51:03 -0400

    feat: add first script; add pallets

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-$("#first-navbar").addClass("js");
+const canvas = document.getElementById('switcher'); // see more information here https://www.youtube.com/watch?v=1NQE6vcVK6M 
+    canvas.width = 512;                                                             // https://www.youtube.com/watch?v=dYjSV5UUxJg 
+    canvas.height = 512;                                                            // https://www.youtube.com/watch?v=0ZJ1Ev7qgmM
+let context = canvas.getContext('2d');
+
+let small_img = [
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+];
+for (let i=0; i<small_img.length; i++) {
+    for (let j=0; j<small_img[i].length; j++) {
+        context.fillStyle = "#" + small_img[i][j];
+        context.fillRect(i*128, j*128, 128, 128);
+    }     
+}
